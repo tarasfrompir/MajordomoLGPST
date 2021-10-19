e74398a7facb80504532fbb890c544da67a1821e
tarasfrompir <tarasfrompir@inbox.ru>
2021-10-19 14:26:22 +0300

    Update SLGPST_structure.php

--- modules/devices/addons/SLGPST_structure.php
+++ modules/devices/addons/SLGPST_structure.php
@@ -2,16 +2,12 @@
 
 $this->device_types['LiveGPSTraker'] = array(
     'TITLE'=>'LiveGPSTraker',
-    'PARENT_CLASS'=>'SDevices',
+    'PARENT_CLASS'=>'SGPS',
     'CLASS'=>'SLGPST',
     'PROPERTIES'=>array(
-        'alive'=>array('DESCRIPTION'=>'Состояние устройства'),
         'timeChek'=>array('DESCRIPTION'=>'Период опроса устройства в секундах','_CONFIG_TYPE'=>'text'),
         'deviceId'=>array('DESCRIPTION'=>'Код устройства','_CONFIG_TYPE'=>'text'),
-        'coordinates'=>array('DESCRIPTION'=>'Координаты расположения устройства'),
-        'speed'=>array('DESCRIPTION'=>'Скорость'),
         'allproperties'=>array('DESCRIPTION'=>'Все свойства устройства'),
-        'address'=>array('DESCRIPTION'=>'Адрес местонахождения устройства'),
         //'d'=>array('DESCRIPTION'=>'Дата обновления статуса'),
         //'t'=>array('DESCRIPTION'=>'Время обновления статуса'),
         //'altitude'=>array('DESCRIPTION'=>'Высота'),
