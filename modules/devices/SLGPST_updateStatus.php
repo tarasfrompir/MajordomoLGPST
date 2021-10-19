2af415c5bf02dedbab7f73adb118c8e01f9447a5
tarasfrompir <tarasfrompir@inbox.ru>
2021-10-19 08:59:29 +0300

    Update SLGPST_updateStatus.php

--- modules/devices/SLGPST_updateStatus.php
+++ modules/devices/SLGPST_updateStatus.php
@@ -2,17 +2,15 @@
 $device_id = $this->getProperty('deviceId');
 $content=getURL('http://livegpstracks.com/viewer_coos_s.php?code='.$device_id,  0); 
 $data=json_decode($content,true);
-//DebMes($content);
 $this->setProperty('coordinates', $data[0]['lat'].','.$data[0]['lng']);
-//$this->setProperty('longitude', $data[0]['lng']); 
 $this->setProperty('speed', $data[0]['speed']);
-//$this->setProperty('d', $data[0]['d']);
-//$this->setProperty('t', $data[0]['t']);
-//$this->setProperty('altitude', $data[0]['altitude']);
 $this->setProperty('allproperties', $content);
-DebMes($this);
+//DebMes($this);
+$adress = geocodingFromCoord($data[0]['lat'], $data[0]['lng']);
+$this->setProperty('address', $adress);
 if (isset($this->linked_user) and $this->linked_user != '') {
     sg($this->linked_user.'.coordinates', $data[0]['lat'].','.$data[0]['lng']);
+    sg($this->linked_user.'.address', $adress);
 }
 // Тут надо вызывать по таймеру этот метод
 if (!$time_to_chek =  $this->getProperty('timeChek') ) $time_to_chek = 60;
